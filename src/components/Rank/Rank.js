c105c82ae1739b2517520a7ee879a625a060f8dd
Caleb <55471287+Carazy1227@users.noreply.github.com>
2019-09-18 21:10:56 -0500

    Add files via upload

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Rank.css';
+
+const Rank = () => {
+    return(
+    <div>
+    <div className=" f3 color ">
+     {'Caleb, Your Current Rank Is....'}
+    </div>
+    <div className='color'>
+        {'#3'}
+    </div>
+    </div>
+    )
+} 
+
+export default Rank;
